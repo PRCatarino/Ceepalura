ce2e232337ed5eacdc229d173081ef5629a08aad
Paulo <paulo061104@gmail.com>
2021-03-01 23:25:52 -0300

    projeto

---
+++ listaDeTarefas.js
@@ -0,0 +1,20 @@
+const criarTarefa = () => {
+
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click',(evento)=> {
+
+    evento.preventDefault()
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value 
+
+    console.log( valor )
+    input.value =" "
+})
+
+document.querySelector('[content]')
+
+
+
